4ce4137aeec48f5bfe1fbf2d603499a84f1d5353
Eron Moraes <eron.moraes7@gmail.com>
2026-01-09 22:00:46 -0300

    adicionando campos

--- js/app.js
+++ js/app.js
@@ -3,32 +3,70 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            teams,
+            teams: window.teams,
+
+            positions: [
+                "Goleiro",
+                "Zagueiro",
+                "Lateral",
+                "Volante",
+                "Meia",
+                "Atacante"
+            ],
+
             players: [],
+
             player: {
                 name: "",
-                team: ""
+                birthDate: "",
+                position: "",
+                foot: "",
+                height: "",
+                weight: "",
+                team: null,
+                status: ""
             },
+
             selectedColor: "transparent"
         };
     },
 
     methods: {
         updateTeamColors() {
-            this.selectedColor = this.player.team.color || "transparent";
+            this.selectedColor = this.player.team
+                ? this.player.team.color
+                : "transparent";
         },
 
         addPlayer() {
-            if (!this.player.name || !this.player.team) return;
+            if (
+                !this.player.name ||
+                !this.player.birthDate ||
+                !this.player.position ||
+                !this.player.foot ||
+                !this.player.height ||
+                !this.player.weight ||
+                !this.player.team ||
+                !this.player.status
+            ) {
+                alert("Preencha todos os campos");
+                return;
+            }
 
-            this.players.push({
-                name: this.player.name,
-                team: this.player.team
-            });
+            this.players.push({ ...this.player });
 
-            // Reset form
-            this.player.name = "";
-            this.player.team = "";
+            // Reset
+            this.player = {
+                name: "",
+                birthDate: "",
+                position: "",
+                foot: "",
+                height: "",
+                weight: "",
+                team: null,
+                status: ""
+            };
+
             this.selectedColor = "transparent";
         }
     }
